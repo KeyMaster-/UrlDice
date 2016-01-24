c778295309926b7b59fcb90f2cb7b559d4364a37
Tilman Schmidt <AccessKeymaster@gmx.net>
2016-01-24 15:16:39 +0000

    Small corrections and some comments

--- src/DiceParser.hx
+++ src/DiceParser.hx
@@ -14,7 +14,7 @@ enum Op {
 
 class DiceLexer extends hxparse.Lexer implements hxparse.RuleBuilder {
     static public var tok = @:rule [
-        "[1-9]+d[0-9]+" => strToRoll(lexer.current),
+        "[1-9]+d[1-9]+" => strToRoll(lexer.current),
         "\\+" => TOperation(plus),
         "\\-" => TOperation(minus),
         "\\*" => TOperation(multiply),
@@ -23,13 +23,14 @@ class DiceLexer extends hxparse.Lexer implements hxparse.RuleBuilder {
     ];
 
     static function strToRoll(str:String) {
-        var regex = ~/(\d+)d(\d+)/;
+        var regex = ~/([1-9]+)d([1-9]+)/;
         regex.match(str);
         return TRoll(Std.parseInt(regex.matched(1)), Std.parseInt(regex.matched(2)));
     }
 }
 
 class DiceParser extends hxparse.Parser<hxparse.LexerTokenSource<DiceToken>, DiceToken> implements hxparse.ParserBuilder {
+        //Record of all individual dice rolls
     public var rolls:Array<Roll>;
     public function new(input:byte.ByteData) {
         rolls = [];
@@ -39,10 +40,12 @@ class DiceParser extends hxparse.Parser<hxparse.LexerTokenSource<DiceToken>, Dic
     }
 
     public function parse() {
-        return parse_numeric(parse_number());
+        return parse_numeric(parse_block()); //Consume the first block since parse_numeric expects an operation
     }
 
-    function parse_number() {
+        //This parses the next "block", which are all numeric parts connected by multiplications (i.e. 2d6*3*5d7 is one block)
+        //This is necessary to ensure order of operations 
+    function parse_block() {
         return switch stream {
             case [TRoll(left, right)]:
                 var dice_result = 0;
@@ -54,33 +57,35 @@ class DiceParser extends hxparse.Parser<hxparse.LexerTokenSource<DiceToken>, Dic
                     });
                     dice_result += res;
                 }
-                parse_mult(dice_result);
+                check_mult(dice_result);
 
             case [TNumber(n)]:
-                parse_mult(n);
+                check_mult(n);
         }
     }
 
-    function parse_mult(left:Int):Int {
+        //Looks ahead in the token stream to see if the next operation is a multiplication
+        //If it is, the block parsing continues, otherwise, we stop and return back to parse_numeric (eventually)
+    function check_mult(left:Int):Int {
         return switch(this.peek(0)) {
             case TOperation(multiply):
                 this.junk();
-                left * parse_number();
+                left * parse_block();
             case _:
                 left;
         }
     }
-
-    public function parse_numeric(total:Int) {
+        //This handles the actual combination of blocks (through + and - operations)
+    function parse_numeric(total:Int) {
         return switch stream {
             case [TOperation(op)]:
                 switch(op) {
                     case plus:
-                        total += parse_number();
+                        total += parse_block();
                         parse_numeric(total);
 
                     case minus:
-                        total -= parse_number();
+                        total -= parse_block();
                         parse_numeric(total);
                     case multiply:
                         trace('This should never be reached...');
